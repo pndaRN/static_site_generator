786adbac68590cd2d1be8dec916f0c08b783511b
pndaRN <markbwilliamsjr@gmail.com>
2025-05-17 16:19:33 -0500

    now on 3.1

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,5 @@
+from textnode import *
+
 class HTMLNode():
     def __init__(self, tag = None, value = None, children = None, props = None):
         self.tag = tag
@@ -26,3 +28,68 @@ class HTMLNode():
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = {}):
+        super().__init__(tag, value, [], props)
+
+    def to_htlm(self):
+        if self.value == "" or self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return self.value
+        if self.props != None:
+            return f'"<{self.tag}{self.props}>{self.value}</{self.tag}>"'
+        else:
+            return f'"<{self.tag}>{self.value}</{self.tag}>"'
+        
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children = []):
+        super().__init__(tag, children)
+
+    def to_html(self):
+        if self.tag == "" or self.tag == None:
+            raise ValueError("No tags given")
+        if self.children == "" or self.children == None or self.children == []:
+            raise ValueError("No children given")
+        
+        result = f"<{self.tag}>"
+        for child in self.children:
+            result += child.to_html()
+        result += f"</{self.tag}>"
+        return result
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+            )
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.NORMAL:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == TextType.LINKS:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"alt":text_node.text, "src":text_node.url})
+    else:
+        raise Exception("Unknown Type or missing type")
+    
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import *
+from textnode import TextNode
 
 class TestHtmlNode(unittest.TestCase):
     def test_to_html_props(self):
@@ -48,6 +49,92 @@ class TestHtmlNode(unittest.TestCase):
             node.__repr__(),
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
+    def test_normal_text(self):
+        """Test converting normal text node to HTML node."""
+        text_node = TextNode("Hello, world!", TextType.NORMAL) 
+        html_node = text_node_to_html_node(text_node)
+        
+        self.assertIsNone(html_node.tag)
+        self.assertEqual(html_node.value, "Hello, world!")
+        self.assertEqual(html_node.props, {})
+ 
+    def test_bold_text(self):
+        text_node = TextNode("Bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(text_node)
+        
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Bold text")
+        self.assertEqual(html_node.props, {})
+ 
+    def test_italic_text(self):
+        text_node = TextNode("Italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(text_node)
+        
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "Italic text")
+        self.assertEqual(html_node.props, {})
+    
+    def test_code_text(self):
+        text_node = TextNode("print('Hello')", TextType.CODE)
+        html_node = text_node_to_html_node(text_node)
+        
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "print('Hello')")
+        self.assertEqual(html_node.props, {})
+
+    def test_link_text(self):
+        text_node = TextNode("Visit Google", TextType.LINKS, url="https://www.google.com")
+        html_node = text_node_to_html_node(text_node)
+        
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Visit Google")
+        self.assertEqual(html_node.props, {"href": "https://www.google.com"})
+
+    def test_image_text(self):
+        text_node = TextNode("Profile picture", TextType.IMAGE, url="https://example.com/image.jpg")
+        html_node = text_node_to_html_node(text_node)
+        
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"alt": "Profile picture", "src": "https://example.com/image.jpg"})
+
+    def test_missing_url_for_link(self):
+        text_node = TextNode("Bad link", TextType.LINKS)
+        html_node = text_node_to_html_node(text_node)
+        
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Bad link")
+        self.assertEqual(html_node.props, {"href": None})
+"""    
+    def test_missing_url_for_image(self):
+        text_node = TextNode("Missing image", TextType.IMAGE)
+        html_node = text_node_to_html_node(text_node)
+        
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.attributes, {"alt": "Missing image", "src": None})
+    
+    def test_empty_text(self):
+        text_node = TextNode("", TextType.NORMAL)
+        html_node = text_node_to_html_node(text_node)
+        
+        self.assertIsNone(html_node.tag)
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.attributes, {})
+    
+    def test_exception_for_unknown_type(self):
+        # Create a text node with a text_type attribute that doesn't match any case
+        class UnknownTextType(Enum):
+            UNKNOWN = auto()
+            
+        text_node = TextNode("Test")
+        text_node.text_type = UnknownTextType.UNKNOWN
+        
+        with self.assertRaises(Exception) as context:
+            text_node_to_html_node(text_node)
+        
+        self.assertTrue("Unknown Type or missing type" in str(context.exception))
+"""
 
 if __name__ == "__main__":
     unittest.main()
